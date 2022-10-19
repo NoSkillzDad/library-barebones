f03e3d75a202b9f1a3f5aa63fe8c4e98e1e4f8f0
Alejandro Peralta <agperalta@gmail.com>
2022-10-19 10:30:40 +0200

    chamnged SearchBar.js from useState to useRef

--- src/components/searchbar/SearchBar.js
+++ src/components/searchbar/SearchBar.js
@@ -1,29 +1,41 @@
 import {useNavigate} from "react-router-dom";
-import {useState} from "react";
+import {useRef, useState} from "react";
 import "./Searchbar.scss"
 
 const SearchBar = () => {
 
     const navigate = useNavigate();
 
-    const [bookId, setBookId] = useState();
+    // const [bookId, setBookId] = useState();
+    const inputRef = useRef();
 
-    const handleChange = (e) => {
-        setBookId(e.target.value);
-    }
+
+    // const handleChange = (e) => {
+    //     setBookId(e.target.value);
+    // }
 
     const handleSubmit = (e) => {
-        if (bookId < 1 || bookId > 100) {
+        e.preventDefault();
+        const value = inputRef.current.value; //added 2nd time
+
+
+        if (value < 1 || value > 100) {
             alert("Enter a valid Book id (1-100)");
             return;
         }
-        navigate(`/books/${bookId}`);
+        navigate(`/books/${value}`);
+
+        // if (bookId < 1 || bookId > 100) {
+        //     alert("Enter a valid Book id (1-100)");
+        //     return;
+        // }
+        // navigate(`/books/${bookId}`);
     }
 
     return (<>
             <form className={"search-container"} onSubmit={handleSubmit}>
-                <input id={"search-bar"} type={"number"} placeholder={"Enter book id..."} value={bookId}
-                       onChange={handleChange}/>
+                {/*<input id={"search-bar"} type={"number"} placeholder={"Enter book id..."} value={bookId} onChange={handleChange}/>*/}
+                <input id={"search-bar"} type={"number"} placeholder={"Enter book id..."} ref={inputRef}/>
                 <button type="submit">Find Book</button>
             </form>
 
